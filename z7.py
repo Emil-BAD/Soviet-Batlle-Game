881a4b1a106dcfa5c0d637c19ff72f60f1e91220
Emil Badretdinov <hat-patfaceit@mail.ru>
2021-12-13 19:36:26 +0300

    123

--- z7.py
+++ z7.py
@@ -17,60 +17,49 @@ def load_image(name, colorkey=None):
     return image
 
 
-class Car(pygame.sprite.Sprite):
-    car = load_image("../data/car.png")
+class BigBoom(pygame.sprite.Sprite):
+    bombs = load_image("../data/bomb.png")
+    boom = load_image("../data/boom.png")
 
-    def __init__(self, width, height, group):
+    def __init__(self, width, height, *group):
         # НЕОБХОДИМО вызвать конструктор родительского класса Sprite.
-        # Это очень важно !!!
-        super().__init__(group)
-        self.image = Car.car
-        self.image_left = self.image = pygame.transform.flip(self.image, True, False)
-        self.rect = self.image.get_rect()
-        self.rect.x = 0
+        # Это очень важно!!!
+        super().__init__(*group)
         self.width = width
         self.height = height
-        self.rect.y = 0
-        self.flag = 0
+        self.image = BigBoom.bombs
+        self.image_boom = BigBoom.boom
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randrange(0 + self.image.get_width(), width - self.image.get_width())
+        self.rect.y = random.randrange(0 + self.image.get_height(), height - self.image.get_height())
 
-    def update(self):
-        speed = 3
-        if self.rect.x == self.width - self.image.get_width():
-            self.flag = 1
-        elif self.rect.x == 0:
-            self.flag = 0
-        if self.flag == 0:
-            self.image = Car.car
-            self.rect.x += speed
-        elif self.flag == 1:
-            self.image = self.image_left
-            self.rect.x -= speed
+    def update(self, event):
+        if event.type == pygame.MOUSEBUTTONDOWN and \
+                self.rect.collidepoint(event.pos):
+            self.image = self.image_boom
 
 
 def main():
     pygame.init()
-    width = 600
-    height = 95
+    width = 500
+    height = 500
     size = (width, height)
     screen = pygame.display.set_mode(size)
     running = True
-    fps = 60
-    clock = pygame.time.Clock()
-    pygame.display.set_caption("Машинка")
     all_sprites = pygame.sprite.Group()
-    Car(width, height, all_sprites)
+    pygame.display.set_caption("Boom them all")
+    for i in range(20):
+        BigBoom(width, height, all_sprites)
     while running:
-        screen.fill(pygame.Color('white'))
+        screen.fill(pygame.Color('black'))
         all_sprites.draw(screen)
-        all_sprites.update()
         for event in pygame.event.get():
+            all_sprites.update(event)
             if event.type == pygame.QUIT:
                 running = False
-        clock.tick(fps)
         pygame.display.flip()
     pygame.quit()
 
 
 if __name__ == "__main__":
     main()
-
